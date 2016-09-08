414afcc512268d0925b27240b398a35e5b5b1f8f
demoii <chenchen.woo@gmail.com>
2016-09-08 15:06:57 +0900

    finishing everything

--- 001_basics.R
+++ 001_basics.R
@@ -3,3 +3,19 @@
 library("ggplot2")
 library("gcookbook")
 
+# read data from csv
+data <- read.csv("datafile.csv")
+# data file does not have header
+data <- read.csv("filename.csv", header=false)
+# manually assign the header names
+names(data) <- c("","","",... )
+
+# set deliminater , string as factor off 
+data <- read.csv("datafile.csv", sep="\t", stringAsFactors=FALSE)
+
+# convert to factor 
+data$col <- factor(data$col)
+str(data)
+
+
+
